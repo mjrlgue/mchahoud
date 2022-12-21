be4f5a15104561432c29395ee5e2eef66cb6254e
CHAHOUD Marwane <marwane.chahoud@orange.com>
2022-12-21 23:27:58 +0100

    update partciles effect

--- js/demo.js
+++ js/demo.js
@@ -6,8 +6,14 @@
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    // dotColor: '#5cbdaa',
+    // lineColor: '#5cbdaa'
+    dotColor: '#ffffff',
+    lineColor: '#ffffff',
+    particleRadius: 2,
+    lineWidth: 0.2,
+    // minSpeedY: 5,
+    minSpeedX: 1
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
